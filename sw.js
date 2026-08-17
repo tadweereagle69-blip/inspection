8a0a30c4a013dd6bc57eb6a11a05b7c501935a9c
tadweereagle69-blip <tadweer.eagle69@gmail.com>
2026-08-17 17:09:20 +0400

    .

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="field-inspection-ultimate-v25-one-finger-scroll";
+const CACHE="field-inspection-ultimate-v26-android-voice-fix";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
